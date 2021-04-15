6dd316c50d70b8ca19e17e888e786e3271f99dae
Mackenzie Howey <Howeymackenzie@gmail.com>
2021-04-15 18:09:44 -0400

    making API call, getting info back :star::rocket::star:

--- src/App.js
+++ src/App.js
@@ -1,9 +1,38 @@
 import "./App.css";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import RocketStats from "./RocketStats.js";
 
 function App() {
+  const [rocket, setRocket] = useState([]);
+
+  useEffect(() => {
+    axios({
+      url: "https://api.spacexdata.com/v4/rockets",
+      method: "GET",
+      dataResponse: "json",
+      params: {},
+    }).then((res) => {
+      setRocket(res.data);
+      console.log(res.data);
+    });
+  }, []);
+
   return (
     <div className='App'>
       <h1>🚀👩‍🚀🚀👨‍🚀🚀BATLLE ROCKETS GOOOOOO 🚀👩‍🚀🚀👨‍🚀🚀</h1>
+      {rocket.map((singleRocket) => {
+        return (
+          <RocketStats
+            name={singleRocket.name}
+            mass={singleRocket.mass.lb}
+            height={singleRocket.height.feet}
+            diameter={singleRocket.diameter.feet}
+            country={singleRocket.country}
+            description={singleRocket.description}
+          />
+        );
+      })}
     </div>
   );
 }
