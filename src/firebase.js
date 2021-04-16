6bace6fbf057aa6169e7e6589353cab6b09efbde
mmmccormack <pmccorma@gmail.com>
2021-04-16 08:33:15 -0400

    about to make production branch  :evergreen_tree:

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAf8Lax6c8QkmYvzHsgmjMxxDzRhdreb9k",
+  authDomain: "battlerockets-5c6fc.firebaseapp.com",
+  projectId: "battlerockets-5c6fc",
+  storageBucket: "battlerockets-5c6fc.appspot.com",
+  messagingSenderId: "827425225449",
+  appId: "1:827425225449:web:82c0909517ed32e07dce78",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+export default firebase;
