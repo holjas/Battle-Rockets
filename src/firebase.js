af2575514c876a1d30da6e1198a5fc024d4c376d
holjas <holly.jasiura@gmail.com>
2021-04-16 13:08:38 -0400

    close windows folks

--- src/firebase.js
+++ src/firebase.js
@@ -7,7 +7,7 @@ var firebaseConfig = {
   projectId: "battlerockets-5c6fc",
   storageBucket: "battlerockets-5c6fc.appspot.com",
   messagingSenderId: "827425225449",
-  appId: "1:827425225449:web:82c0909517ed32e07dce78",
+  // appId: "1:827425225449:web:82c0909517ed32e07dce78",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
