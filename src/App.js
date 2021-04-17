2d60820f55d1fab3116cb354f2740de42a6bafa8
holjas <holly.jasiura@gmail.com>
2021-04-16 21:43:34 -0400

    this is a good place right here

--- src/App.js
+++ src/App.js
@@ -7,99 +7,74 @@ import Lobby from "./Lobby";
 
 function App() {
   const [data, setData] = useState({});
-  const [token, setToken] = useState(null);
 
-  //create a token when component first mounts
+  //pull from firebase what's there (runs when token is assigned)
   useEffect(() => {
-    //generate a token, borrowed from somewhere off the internet
-    function create_UUID() {
-      var dt = new Date().getTime();
-      var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
-        /[xy]/g,
-        function (c) {
-          var r = (dt + Math.random() * 16) % 16 | 0;
-          dt = Math.floor(dt / 16);
-          return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
-        }
-      );
-      return uuid;
-    }
-    setToken(create_UUID());
-  }, []);
-
-  useEffect(() => {
-    //pull from firebase what's there (runs when token is assigned)
     const dbRef = firebase.database().ref();
     dbRef.on("value", (response) => {
       setData(response.val());
+      // console.log(response.val());
     });
-  }, [token]);
+  }, []);
+
+  const playerOne = data.playerOne;
+
+  // const playerTwo = data.playerTwo;
 
   //destructure the data returned from firebase,
-  const { players = {} } = data;
+  // const { players = {} } = data;
   //checking that two players have 'entered' the game
-  const enoughPlayers = Object.keys(players).length === 2;
+  // const enoughPlayers = Object.keys(players).length === 2;
   //when two player are confirmed, push P1/P2 info to firebase
-  useEffect(() => {
-    if (enoughPlayers) {
-      const [playerOne, playerTwo] = Object.values(players);
-      const pOne = firebase.database().ref().child("playerOne");
-      const pTwo = firebase.database().ref().child("playerTwo");
+  // useEffect(() => {
+  //   if (enoughPlayers) {
+  //     const [playerOne, playerTwo] = Object.values(players);
+  //     const pOne = firebase.database().ref().child("playerOne");
+  //     const pTwo = firebase.database().ref().child("playerTwo");
 
-      pOne.push(playerOne);
-      pTwo.push(playerTwo);
-    }
+  //     pOne.push(playerOne);
+  //     pTwo.push(playerTwo);
+  //   }
 
-    // for (const key in data.playerOne) {
-    //   console.log(data.playerOne[key].token);
-    // }
+  //   // for (const key in data.playerOne) {
+  //   //   console.log(data.playerOne[key].token);
+  //   //   if (data.playerOne[key].token === token) console.log("Iam player one");
+  //   // }
 
-    // for (const key in data.playerTwo) {
-    //   console.log(data.playerTwo[key].token);
-    //   const firebaseToken = data.playerTwo[key].token;
-    //   if (firebaseToken === token) {
-    //     console.log("IAM PLAYER TWO");
-    //   }
-    // }
-  }, [enoughPlayers]);
+  //   // for (const key in data) {
+  //   //   console.log(data.playerOne[key].token, "FIREBASE TOKEN");
+  //   //   console.log(token, "REGULAR TOKEN");
+  //   //   const firebaseToken = data.playerOne[key].token;
+  //   //   if (firebaseToken === token) {
+  //   //     console.log("IAM PLAYER ONE");
+  //   //   }
+  //   // }
+  // }, [enoughPlayers]);
 
-  for (const key in data.playerOne) {
-    console.log(data.playerOne[key].token, "FIREBASE TOKEN");
-    console.log(token, "REGULAR TOKEN");
-    const firebaseToken = data.playerOne[key].token;
-    if (firebaseToken === token) {
-      console.log("IAM PLAYER ONE");
-    }
-  }
-  for (const key in data.playerTwo) {
-    console.log(data.playerTwo[key].token, "FIREBASE TOKEN");
-    console.log(token, "REGULAR TOKEN");
-    const firebaseToken = data.playerTwo[key].token;
-    if (firebaseToken === token) {
-      console.log("IAM PLAYER TWO");
-    }
-  }
+  // for (const key in data.playerOne) {
+  //   console.log(data.playerOne[key].token);
+  //   if (data.playerOne[key].token === token) console.log("Iam player one");
+  // }
 
   //Game Start component to capture user name
-  const addPlayer = (name) => {
-    const dbRef = firebase.database().ref().child("players");
-    dbRef.push({
-      playerName: name,
-      token: token,
-    });
-  };
+  // const addPlayer = (name) => {
+  //   const dbRef = firebase.database().ref().child("players");
+  //   dbRef.push({
+  //     playerName: name,
+  //     token: token,
+  //   });
+  // };
 
   // button for clearing firebase. testing only!!!!!
   const removeEverything = () => {
-    firebase.database().ref("playerOne").remove();
-    firebase.database().ref("playerTwo").remove();
-    firebase.database().ref("players").remove();
+    firebase.database().ref("playerOne").set(false);
+    firebase.database().ref("playerTwo").set(false);
   };
   return (
     <div className="App">
       <h1>🚀👩‍🚀🚀👨‍🚀🚀BATLLE ROCKETS GOOOOOO 🚀👩‍🚀🚀👨‍🚀🚀</h1>
 
-      <GameStart addPlayer={addPlayer} player1={{}} player2={{}} />
+      <GameStart playerOne={playerOne} />
 
       <Lobby />
       {/* Button for testing only */}
--- src/GameStart.js
+++ src/GameStart.js
@@ -1,15 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import firebase from "./firebase";
 
 function GameStart(props) {
-  // const [isPlayerOneReady, setIsPlayerOneReady] = useState(false);
-  // const [isPlayerTwoReady, setIsPlayerTwoReady] = useState(false);
+  const { playerOne } = props;
+
   const [playerOneName, setPlayerOneName] = useState("");
   const [playerTwoName, setPlayerTwoName] = useState("");
-  const { addPlayer } = props;
+  const [token, setToken] = useState(null);
 
   //handleClick, to confirm when players are ready. Also captures their names.
-  const handleIsPlayerReady = (player) => {
-    addPlayer(player);
+  const handleIsPlayerReady = (player, playerNumber) => {
+    firebase.database().ref().child(playerNumber).push({
+      name: player,
+      token: token,
+    });
   };
   //captures text input
   const handleChange = (event, playerNumber) => {
@@ -20,37 +24,64 @@ function GameStart(props) {
       setPlayerTwoName(event.target.value);
     }
   };
+  //create a token when component first mounts
+  useEffect(() => {
+    //generate a token, borrowed from somewhere off the internet
+    function create_UUID() {
+      var dt = new Date().getTime();
+      var uuid = "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(
+        /[xy]/g,
+        function (c) {
+          var r = (dt + Math.random() * 16) % 16 | 0;
+          dt = Math.floor(dt / 16);
+          return (c === "x" ? r : (r & 0x3) | 0x8).toString(16);
+        }
+      );
+      return uuid;
+    }
+    setToken(create_UUID());
+  }, []);
 
   return (
     <section>
-      <h2>game start</h2>
-      <input
-        type="text"
-        onChange={(event) => handleChange(event, "one")}
-        value={playerOneName}
-      ></input>
-      <button
-        onClick={() => {
-          handleIsPlayerReady(playerOneName);
-          setPlayerOneName("");
-        }}
-      >
-        player one
-      </button>
+      {!playerOne && (
+        <>
+          <p>welcome player one! please enter your name</p>
+          <input
+            type="text"
+            onChange={(event) => handleChange(event, "one")}
+            value={playerOneName}
+          ></input>
+          <button
+            onClick={() => {
+              handleIsPlayerReady(playerOneName, "playerOne");
+              setPlayerOneName("");
+            }}
+          >
+            player one
+          </button>
+        </>
+      )}
+      {playerOne && (
+        <div>
+          <p>Player One {playerOneName} has already entered the game.</p>
+          <p>Waiting for player two to enter the game...</p>
 
-      <input
-        type="text"
-        onChange={(event) => handleChange(event, "two")}
-        value={playerTwoName}
-      ></input>
-      <button
-        onClick={() => {
-          handleIsPlayerReady(playerTwoName);
-          setPlayerTwoName("");
-        }}
-      >
-        player two
-      </button>
+          <input
+            type="text"
+            onChange={(event) => handleChange(event, "two")}
+            value={playerTwoName}
+          ></input>
+          <button
+            onClick={() => {
+              handleIsPlayerReady(playerTwoName, "playerTwo");
+              setPlayerTwoName("");
+            }}
+          >
+            player two
+          </button>
+        </div>
+      )}
     </section>
   );
 }
