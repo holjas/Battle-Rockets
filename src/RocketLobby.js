6435d3a12d9fba64f792d045fc94bf1763d003d9
holjas <holly.jasiura@gmail.com>
2021-04-18 19:50:44 -0400

    stuffs

--- src/RocketLobby.js
+++ src/RocketLobby.js
@@ -1,45 +1,71 @@
 import "./Rockets.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
-import firebase from "./firebase";
 import rocket1 from "./images/rocket-1.png";
+import rocket2 from "./images/rocket-2.png";
+import rocket3 from "./images/rocket-3.png";
 
-function RocketLobby() {
+function Rockets() {
   const [rocket, setRocket] = useState([]);
 
   useEffect(() => {
     axios({
-      url: "https://api.spacexdata.com/v4/rockets",
+      url: "https://api.spacexdata.com/v3/rockets/",
       method: "GET",
       dataResponse: "json",
       params: {},
-    }).then((res) => {
-      setRocket(res.data);
-    });
+    })
+      .then((res) => {
+        const rocketHeight = res.data.map((rHeight) => {
+          const singleRocketHeight = rHeight.height.meters;
+          console.log(singleRocketHeight);
+          let orientation = rocket3;
+          if (singleRocketHeight > 100) {
+            orientation = rocket1;
+          } else if (singleRocketHeight > 50) {
+            orientation = rocket2;
+          } else {
+            orientation = rocket3;
+          }
+          return {
+            ...rHeight,
+            orientation: orientation,
+          };
+        });
+        setRocket(rocketHeight);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }, []);
-
-  const handleRocketSelected = () => {
-    firebase.database().ref("playerOne").child("rockets").set({
-      rocketSelectedOne: "I'm rocket one",
-    });
-  };
 
   return (
     <div>
       <h3>Choose Three Rockets as your game pieces </h3>
-
       <form className="style">
-        <input type="checkbox" name="rocket1" onClick={handleRocketSelected} />
-        <label htmlFor="rocket1">
-          <img className="rocket1" src={rocket1} alt="rocket icon" />
-        </label>
         {rocket.map((singleRocket, index) => {
           return (
             <div key={index}>
-              <p>Rocket Name: {singleRocket.name}</p>
+              <label
+                className="visually-hidden"
+                htmlFor={singleRocket.rocket_id}
+              >
+                {singleRocket.rocket_name}
+              </label>
+              <input
+                type="checkbox"
+                id={singleRocket.rocket_id}
+                name={singleRocket.rocket_id}
+              />
+              <img
+                className="rocket1"
+                src={singleRocket.orientation}
+                alt={singleRocket.rocket_name}
+              />
+              <p>Rocket Name: {singleRocket.rocket_name}</p>
               <p>Diameter: {singleRocket.diameter.feet}</p>
-              <p>Description: {singleRocket.description}</p>
-              <p>Country:{singleRocket.description}</p>
+              <p>Country: {singleRocket.country}</p>
+              <p>Description:{singleRocket.description}</p>
             </div>
           );
         })}
@@ -49,4 +75,4 @@ function RocketLobby() {
   );
 }
 
-export default RocketLobby;
+export default Rockets;
